88fc154a9f0634a0446316228c5f3c1a9b923174
MuratY2 <murat.yazgan@ozu.edu.tr>
2024-12-21 00:46:32 +0300

    add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeXYzZPfXQZS_8EdSQujMzC8o4GBvMID0",
+  authDomain: "ozyeginchat.firebaseapp.com",
+  projectId: "ozyeginchat",
+  storageBucket: "ozyeginchat.appspot.com", 
+  messagingSenderId: "246402583164",
+  appId: "1:246402583164:web:dcdacce93f985b227a57f9",
+  measurementId: "G-DYFGKTKF0V",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export { app, auth, firestore, analytics };
